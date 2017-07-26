32f1bcd96c4ea2ec7e4c351cd0c391a3d0f8b29b
huftyuk <github@stevehoughton.org.uk>
2017-07-26 19:34:09 +0100

    Update TS_Fiddle.py

--- TS_Fiddle.py
+++ TS_Fiddle.py
@@ -1,9 +1,10 @@
-import urllib2
+import urllib
 import time
 i = 1;
 baseURL = 'https://api.thingspeak.com/update?api_key=CA3YNXBUBJTKRLVH'
 
 while 1:
-  f = urllib2.urlopen(baseURL + 'field1=73')
+  f = urllib.urlopen("https://api.thingspeak.com/update?key=CA3YNXBUBJTKRLVH&field1=73")
+  print f.read()
   time.sleep(16)
   i = i+1
