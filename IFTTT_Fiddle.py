59aab79be57155733ecef1c6961bfe1b42759081
Ubuntu <ubuntu@ip-172-31-11-226.eu-west-2.compute.internal>
2017-07-26 17:53:08 +0000

    Correct use of urllib2

--- IFTTT_Fiddle.py
+++ IFTTT_Fiddle.py
@@ -1,3 +1,3 @@
 import urllib2
 
-urllib2.open('https://maker.ifttt.com/trigger/%22bingo%22/with/key/fF_oXNFLzvmF_Rlpn1_NDiabvQobeCYJ_QVfX39DqbV')
+urllib2.urlopen('https://maker.ifttt.com/trigger/%22bingo%22/with/key/fF_oXNFLzvmF_Rlpn1_NDiabvQobeCYJ_QVfX39DqbV')
