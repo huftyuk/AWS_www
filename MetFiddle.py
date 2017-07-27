0139c10c7d2b684673e9b4c390f073812e806632
huftyuk <github@stevehoughton.org.uk>
2017-07-27 07:37:22 +0100

    Update MetFiddle.py

--- MetFiddle.py
+++ MetFiddle.py
@@ -5,6 +5,13 @@ import sys
 sys.path.append('/home/ubuntu')
 import APIKeyManager
 
+
+import urllib
+import time
+i = 1;
+baseURL = 'https://api.thingspeak.com/update?api_key=CA3YNXBUBJTKRLVH'
+
+  
 apikey = APIKeyManager.MetDataPoint
 
 
@@ -15,12 +22,19 @@ conn = datapoint.Manager(api_key=apikey)
 site = conn.get_nearest_site(-0.124626, 51.500728)
 print site.name
 
-# Get a forecast for the nearest site
-forecast = conn.get_forecast_for_site(site.id, "3hourly")
+while 1:
+  # Get a forecast for the nearest site
+  forecast = conn.get_forecast_for_site(site.id, "3hourly")
 
-# Get the current timestep using now() and print out some info
-now = forecast.now()
-print now.weather.text
-print "%s%s%s" % (now.temperature.value,
+  # Get the current timestep using now() and print out some info
+  now = forecast.now()
+  print now.weather.text
+  print "%s%s%s" % (now.temperature.value,
                   u'\xb0', #Unicode character for degree symbol
                   now.temperature.units)
+  urlstring = "https://api.thingspeak.com/update?key=CA3YNXBUBJTKRLVH&field1=" + now.temperature.value
+  f = urllib.urlopen(urlstring)
+  print f.read()
+  time.sleep(16)
+  i = i+1
+
